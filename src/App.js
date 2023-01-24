29197ed13997debe8923aa93e5be94aec33e6432
Erhan <erhankubanych@gmail.com>
2023-01-24 16:51:18 +0600

    delite

--- src/App.js
+++ src/App.js
@@ -1,65 +1,9 @@
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { addCustomerAction, removeCustomerAction } from './store/customerReducer'
-// import State from './components/State'
 
 const App = () => {
-
-	const dispatch = useDispatch()
-	const cash = useSelector(state => state.cash.cash)
-	const customers = useSelector(state => state.customers.customers)
-
-	const addCash = (cash) => {
-		dispatch({ type:'ADD_CASH', payload: cash })
-	}
-
-	const getCash = (cash) => {
-		dispatch({ type: 'GET_CASH', payload: cash })
-	}
-
-	const addCustomer = (name) => { 
-		const customer = {
-			name,
-			id: Date.now(),
-		}
-		dispatch(addCustomerAction(customer))
-	}
-
-	const removeCustomer = (customer) => { 
-		dispatch( removeCustomerAction(customer.id))
-	}
-	
-
-	return (
-		<div>
-			{/* <State /> */}
-			<div className='app'>
-				<div style={{ fontSize: "3rem" }}>{cash}</div>
-				<div style={{ display: "flex" }}>
-					<button onClick={() => addCash(Number(prompt()))}>пополнить счет</button>
-					<button onClick={() => getCash(Number(prompt()))}>снять со счета</button>
-					<button onClick={() => addCustomer(prompt())}>добавить клиента</button>
-				</div>
-				<div>
-					{
-						customers.length > 0 ? (
-							<div>
-								{customers.map(customer => 
-									<dir key={customer.id}>
-										<p onClick={()=> removeCustomer(customer)} >{customer.name}</p>
-									</dir>
-								)}
-							</div>
-						) : (
-								<div>
-									<p>клиенты отсутсвуют !!!</p>
-								</div>
-						)
-					}
-				</div>
-			</div>
-		</div>
-	)
+  return (
+    <div>App</div>
+  )
 }
 
 export default App
